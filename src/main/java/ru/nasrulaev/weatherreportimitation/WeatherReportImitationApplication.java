0db9e9071611d4a6a041d471b17349e0ff93bca0
mafuba <tulkidg@gmail.com>
2023-10-01 13:51:48 +0300

    Initial commit

---
+++ src/main/java/ru/nasrulaev/weatherreportimitation/WeatherReportImitationApplication.java
@@ -0,0 +1,13 @@
+package ru.nasrulaev.weatherreportimitation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherReportImitationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherReportImitationApplication.class, args);
+	}
+
+}
