8c081809a8e5f3484cfa68d223bf8de2047ded60
mafuba <tulkidg@gmail.com>
2023-10-02 18:45:48 +0300

    chore: create SensorNotCreatedException

---
+++ src/main/java/ru/nasrulaev/weatherreportimitation/util/errors/SensorNotCreatedException.java
@@ -0,0 +1,9 @@
+package ru.nasrulaev.weatherreportimitation.util.errors;
+
+
+public class SensorNotCreatedException extends RuntimeException{
+    public SensorNotCreatedException(String msg){
+        super(msg);
+    }
+
+}
