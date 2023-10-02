985b2de284c43c3b220fc273d8ba272cef0762db
mafuba <tulkidg@gmail.com>
2023-10-02 19:57:44 +0300

    feat(sensors): add request for sensor registration

--- src/main/java/ru/nasrulaev/weatherreportimitation/controllers/SensorsController.java
+++ src/main/java/ru/nasrulaev/weatherreportimitation/controllers/SensorsController.java
@@ -1,7 +1,58 @@
 package ru.nasrulaev.weatherreportimitation.controllers;
 
+import jakarta.validation.Valid;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.nasrulaev.weatherreportimitation.dto.SensorDTO;
+import ru.nasrulaev.weatherreportimitation.models.Sensor;
+import ru.nasrulaev.weatherreportimitation.services.SensorsService;
+import ru.nasrulaev.weatherreportimitation.util.errors.SensorNotCreatedException;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/sensors")
 public class SensorsController {
+
+    private final SensorsService sensorsService;
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public SensorsController(SensorsService sensorsService, ModelMapper modelMapper) {
+        this.sensorsService = sensorsService;
+        this.modelMapper = modelMapper;
+    }
+
+    @PostMapping("/registration")
+    public ResponseEntity<HttpStatus> register(@RequestBody @Valid SensorDTO sensorDTO,
+                                               BindingResult bindingResult){
+        if (bindingResult.hasErrors()) {
+            StringBuilder errorMsg = new StringBuilder();
+
+            List<FieldError> errors = bindingResult.getFieldErrors();
+            for (FieldError error : errors) {
+                errorMsg.append(error.getField())
+                        .append(" - ")
+                        .append(error.getDefaultMessage())
+                        .append(";");
+            }
+
+            throw new SensorNotCreatedException(errors.toString());
+        }
+
+        sensorsService.save(convertToSensor(sensorDTO));
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    private Sensor convertToSensor(SensorDTO sensorDTO) {
+        return modelMapper.map(sensorDTO, Sensor.class);
+    }
 }
--- src/main/java/ru/nasrulaev/weatherreportimitation/services/SensorsService.java
+++ src/main/java/ru/nasrulaev/weatherreportimitation/services/SensorsService.java
@@ -1,8 +1,21 @@
 package ru.nasrulaev.weatherreportimitation.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.nasrulaev.weatherreportimitation.models.Sensor;
+import ru.nasrulaev.weatherreportimitation.repositories.SensorsRepository;
 
 @Service
 public class SensorsService {
+    private final SensorsRepository sensorsRepository;
+
+    @Autowired
+    public SensorsService(SensorsRepository sensorsRepository) {
+        this.sensorsRepository = sensorsRepository;
+    }
+
+    public void save(Sensor sensor){
+        sensorsRepository.save(sensor);
+    }
 }
 
